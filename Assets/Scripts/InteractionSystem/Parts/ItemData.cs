209d164c1761148c8a269dfce58c778c3cc7f2fa
AidenZChristopher <azchristophe@ualr.edu>
2026-05-06 19:43:34 -0500

    Added function to rotate parts orientation when placing on workbench. This was needed for case to be sideways on workbench but upright when placing on ground.

--- Assets/Scripts/InteractionSystem/Parts/ItemData.cs
+++ Assets/Scripts/InteractionSystem/Parts/ItemData.cs
@@ -27,7 +27,9 @@ public class ItemData : MonoBehaviour, IInteractable
     [SerializeField] private string promptText;
     [SerializeField] private Sprite icon;
     [SerializeField] private float groundOffset = .25f; //default offset value
+    [SerializeField] private Vector3 placementRotation = Vector3.zero; //needed to rotate case 90 degrees on placement
     public float GroundOffset => groundOffset;
+    public Quaternion PlacementRotation => Quaternion.Euler(placementRotation);
 
     //  IInteractable Properties    //
     public string GetPromptText => promptText;
--- Assets/Scripts/InteractionSystem/Workbench/WorkBenchSlot.cs
+++ Assets/Scripts/InteractionSystem/Workbench/WorkBenchSlot.cs
@@ -1,29 +1,30 @@
 using UnityEngine;
-
+//Claude AI was used to generate part of this code. It was then edited by me to fit the needs of the project.
 public class WorkbenchSlot : MonoBehaviour
 {
     public bool isOccupied = false;
     public GameObject occupyingItem = null;
 
-    public void PlaceItem(GameObject item)
+public void PlaceItem(GameObject item)
+{
+    item.SetActive(true);
+    item.transform.position = transform.position;
+
+    ItemData itemData = item.GetComponent<ItemData>();
+    item.transform.rotation = itemData != null ? itemData.PlacementRotation : Quaternion.identity;
+
+    if (itemData != null) itemData.enabled = false;
+
+    if (item.TryGetComponent<DraggablePart>(out var part))
     {
-        item.SetActive(true);
-        item.transform.position = transform.position;
-        item.transform.rotation = Quaternion.identity;
-
-        if (item.TryGetComponent<ItemData>(out var itemData))
-            itemData.enabled = false;
-
-        if (item.TryGetComponent<DraggablePart>(out var part))
-        {
-            part.SetCurrentSlot(this);
-            var workbench = FindFirstObjectByType<WorkBenchInteraction>();
-            if (workbench != null) part.SetWorkbenchCam(workbench.WorkbenchCamera);
-        }
-
-        occupyingItem = item;
-        isOccupied = true;
+        part.SetCurrentSlot(this);
+        var workbench = FindFirstObjectByType<WorkBenchInteraction>();
+        if (workbench != null) part.SetWorkbenchCam(workbench.WorkbenchCamera);
     }
+
+    occupyingItem = item;
+    isOccupied = true;
+}
 
     public void ClearSlot()
     {
